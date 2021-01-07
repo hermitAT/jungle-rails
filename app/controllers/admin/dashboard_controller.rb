ad5b0fd6c4d95abc5b587e1c933cfaab1d101d63
hermitAT <ahthornev2@gmail.com>
2021-01-06 23:09:54 -0800

    Edit link on admin dashboard for admin/categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,5 @@ class Admin::DashboardController < ApplicationController
   def show
     @num_products = Product.count
     @num_categories = Category.count
-    @category = Category.first
   end
 end
