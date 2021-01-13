235bbcfbcb3723f226e7d350cd5717a90f907af6
hermitAT <ahthornev2@gmail.com>
2021-01-12 18:16:54 -0800

    Add tests for user login experience, and fix some bugs in login NAV and auth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  def authenticate_with_credentials(email, password)
+  def self.authenticate_with_credentials(email, password)
     clean_email = email.strip.downcase
 
     @user = User.find_by_email(clean_email)
